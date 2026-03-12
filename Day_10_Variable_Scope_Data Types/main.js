c22d389096454625c21aedd39a86d51cedad1289
vutungvn <tungvuvanthanh@gmail.com>
2026-03-12 16:00:14 +0700

    feat(variables, scope, data types): learn 8 data types in js

--- Day_10_Variable_Scope_Data Types/main.js
+++ Day_10_Variable_Scope_Data Types/main.js
@@ -64,3 +64,55 @@ if (true) {
 // myFunction();
 
 // console.log(number);
+
+// Data types
+// number
+let age = 36;
+console.log("age::", age);
+
+// string
+let name = "John";
+console.log("name::", name);
+
+// boolean
+const isstudent = true;
+console.log("isStudent::", isstudent);
+
+// undefined
+let x;
+console.log("x::", x);
+console.log("typeof x::", typeof x);
+
+// null
+const nothing = null;
+console.log("nothing::", nothing);
+console.log("typeof nothing::", typeof nothing); //Lỗi
+
+// Symbol
+const id = Symbol(1);
+const id2 = Symbol(1);
+console.log(id === id2);
+
+// BigInt
+const bigInt = 1234567890123456789012345678n;
+console.log("bigInt::", bigInt);
+
+// Object
+const user = {
+  name: "Alice",
+  age: 30,
+  address: "123 Main St",
+  isActive: true,
+};
+
+const key = "name"; // Dynamic key
+console.log(user[key]);
+
+console.log(user.name);
+console.log(user.age);
+console.log(user.address);
+console.log(user.isActive);
+
+// Array
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers[1]);
