a82204e11c67f22627cc2d402e0da8c50bf41675
vutungvn <tungvuvanthanh@gmail.com>
2026-05-27 21:05:39 +0700

    learn about asynchronous part 2

--- Day_18_Asynchronous/main.js
+++ Day_18_Asynchronous/main.js
@@ -81,16 +81,72 @@
 
 // console.log("D");
 
-console.log(1);
+// console.log(1);
 
-setTimeout(() => console.log(2));
+// setTimeout(() => console.log(2));
 
-Promise.resolve().then(() => console.log(3));
+// Promise.resolve().then(() => console.log(3));
 
-Promise.resolve().then(() => setTimeout(() => console.log(4)));
+// Promise.resolve().then(() => setTimeout(() => console.log(4)));
 
-Promise.resolve().then(() => console.log(5));
+// Promise.resolve().then(() => console.log(5));
 
-setTimeout(() => console.log(6));
+// setTimeout(() => console.log(6));
 
-console.log(7);
+// console.log(7);
+
+// Promise.all
+
+// const p1 = Promise.resolve(1);
+// const p2 = Promise.resolve(2);
+// const p3 = Promise.reject("Lỗi xảy ra");
+
+// Promise.all([p1, p2, p3])
+//   .then((values) => console.log(values)) // [1, 2, 3]
+//   .catch((error) => console.log(error));
+
+// const fetchPosts = fetch("https://jsonplaceholder.typicode.com/posts");
+// const fetchUsers = fetch("https://jsonplaceholder.typicode.com/users");
+
+// Promise.all([fetchPosts, fetchUsers])
+//   .then((data) => {
+//     const [postsResponse, usersResponse] = data;
+//     postsResponse.json().then((posts) => console.log(posts));
+//     usersResponse.json().then((users) => console.log(users));
+//   })
+//   .catch((error) => console.log("Lỗi xảy ra:", error));
+
+// const p1 = Promise.resolve("Thành công");
+// const p2 = Promise.resolve("Thành công");
+// const p3 = Promise.reject("Thất bại");
+
+// Promise.allSettled([p1, p2, p3])
+//   .then((res) => {
+//     res.forEach((item, index) => {
+//       if (item.status === "fulfilled") {
+//         console.log(
+//           `Vị trí thành công thứ ${index + 1}, Kết quả: ${item.value}`,
+//         );
+//       } else {
+//         console.log(
+//           `Vị trí thành công thứ ${index + 1}, Kết quả: ${item.reason}`,
+//         );
+//       }
+//     });
+//   })
+//   .catch((err) => console.log(err));
+
+// Async/Await
+
+const fetchData = async () => {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await res.json();
+
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+fetchData();
